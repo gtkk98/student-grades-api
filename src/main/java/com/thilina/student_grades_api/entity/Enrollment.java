c90dd0c339f49a4caa538ed78e1944b71e7e81c7
Thilina Kumarasiri <119777366+thilina25@users.noreply.github.com>
2026-03-19 15:22:15 +0530

    Enrollment entity has done.

--- src/main/java/com/thilina/student_grades_api/entity/Enrollment.java
+++ src/main/java/com/thilina/student_grades_api/entity/Enrollment.java
@@ -1,4 +1,38 @@
 package com.thilina.student_grades_api.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "enrollments",
+            // Prevent duplicate enrollment at DB level
+        uniqueConstraints = @UniqueConstraint(
+                columnNames = {"student_id", "course_id"}
+        )
+    )
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Enrollment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // Many enrollment one student
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "student_id", nullable = false)
+    private Student student;
+
+    // Many enrollment one course
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "course_id", nullable = false)
+    private Course course;
+
+    private Double grade;
+
+    private String semester;
+
 }
