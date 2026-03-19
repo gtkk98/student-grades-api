eec1e7773468d20d1946b2f0fd3f8d8ce1f19d5d
Thilina Kumarasiri <119777366+thilina25@users.noreply.github.com>
2026-03-19 15:08:21 +0530

    Connect to the Database (PostgreSQL)

---
+++ src/test/java/com/thilina/student_grades_api/StudentGradesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thilina.student_grades_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentGradesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
